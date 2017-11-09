1c332e65dde785f9bb940ac614b4dc53479494f2
Jacob Harrison <jaccharrison@gmail.com>
2017-11-09 07:54:57 -0600

    Re-simplified, can read and export image

--- DrawApp.java
+++ DrawApp.java
@@ -2,132 +2,110 @@ package draw;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.*;
+import javafx.scene.canvas.*;
 import javafx.stage.*;
 import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.event.*;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javax.imageio.ImageIO;
-
+import javafx.embed.swing.SwingFXUtils;
 import java.io.File;
 import java.net.URI;
 import java.net.URL;
 import java.lang.String;
-import javax.imageio.ImageIO;
-
-import draw.stage.layer.LayerManager;
+import java.nio.ByteBuffer;
 
 public final class DrawApp extends Application {
 
-  private Stage primaryStage; //main window
-  private Scene primaryScene;
+  private Stage drawStage; //main window
+  private Scene drawScene;
   private BorderPane root; //organizes main stage
 
   private MenuBar menuBar;
-  private LayerManager layerManager;
 
-  private Boolean saveState; //true if no unsaved changes
+  private Canvas canvas;
+  private GraphicsContext gc;
 
-  /* start: build interface */
-  public final void start(Stage primaryStage) {
+  /* start: build UI */
+  public final void start(Stage drawStage) {
 
-    primaryStage.setTitle("Draw");
-    primaryStage.setMinHeight(100.0);
-    primaryStage.setMinWidth(150.0);
+    drawStage.setTitle("Draw");
+    drawStage.setMinHeight(200.0);
+    drawStage.setMinWidth(250.0);
 
-    /* building blocks of window */
     root = new BorderPane();
-    primaryScene = new Scene(root);
-    primaryStage.setScene(primaryScene);
+    drawScene = new Scene(root, 500.0, 500.0);
+    drawStage.setScene(drawScene);
 
     /* add menu */
-    buildMenu();
+    buildMenuBar();
     root.setTop(menuBar); //add menu to top of scene
 
-    /* instantiate layerManager */
-    layerManager = new LayerManager(primaryStage);
+    /* add canvas */
+    canvas = new Canvas(400.0, 400.0);
+    gc = canvas.getGraphicsContext2D();
+    root.setCenter(canvas);
 
-    primaryStage.show(); // show window
+    drawStage.sizeToScene();
+    drawStage.show(); // show window
   }
 
-  /* buildMenu: build MenuBar with EventHandlers */
-  private final void buildMenu() {
+  /* buildMenu: build MenuBar and register EventHandlers */
+  private final void buildMenuBar() {
 
-    /* 'file' menu */
-    final Menu fileMenu = new Menu("File"); //menu entry
-    MenuItem newItem = new MenuItem("New Project"); //File > New Project
-    newItem.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent e) {
-      }
-    });
-    MenuItem openItem = new MenuItem("Open Image"); //File > Open Image
+    final Menu fileMenu = new Menu("File");
+    final Menu drawMenu = new Menu("Draw");
+
+    // File menu items
+    MenuItem openItem = new MenuItem("Open Image");
     openItem.setOnAction(new EventHandler<ActionEvent>() {
       @Override
-      /* select and open image from filesystem as project layer */
       public void handle(ActionEvent e) {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Image");
         fileChooser.getExtensionFilters().addAll(
             new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
             new ExtensionFilter("All Files", "*.*"));
         /* get File to be opened */
-        File imgFile = fileChooser.showOpenDialog(primaryStage);
+        File imgFile = fileChooser.showOpenDialog(drawStage);
         if (imgFile != null) {
           try {
-            /* convert to Image and send to layerManager */ 
-            layerManager.newLayer(new Image(imgFile.toURI().toURL().toString()));
-          } catch (Exception ex) {} //TODO: implement catch
+            Image img = new Image(imgFile.toURI().toURL().toString());
+            canvas.setHeight(img.getHeight());
+            canvas.setWidth(img.getWidth());
+            PixelReader pixelReader = img.getPixelReader();
+            PixelWriter pixelWriter = gc.getPixelWriter();
+            PixelFormat<ByteBuffer> pixelFormat = PixelFormat.getByteRgbInstance();
+            gc.drawImage(img, 0.0, 0.0);
+          } catch (Exception ex) {}
         }
       }
     });
-    MenuItem exportItem = new MenuItem("Export"); //File > Export...
+
+    MenuItem exportItem = new MenuItem("Export");
     exportItem.setOnAction(new EventHandler<ActionEvent>() {
-     @Override
-     public void handle(ActionEvent e) {
-       FileChooser fileChooser = new FileChooser();
-       fileChooser.setTitle("Save Image");
-       fileChooser.getExtensionFilters().addAll(
-           // TODO: implement different filetype exports
-//           new ExtensionFilter("JPEG", "*.jpg");
-           new ExtensionFilter("PNG", "*.png"));
-//           new ExtensionFilter("GIF", "*.gif");
-//           new ExtensionFilter("BMP", "*.bmp"));
-       File save = fileChooser.showSaveDialog(primaryStage);
-       if (save != null) {
-         try {
-           ImageIO.write(SwingFXUtils.fromFXImage(layerManager.getExport(),
-                 null), "png", save);
-         } catch (Exception ex) {} //TODO: impelement catch
-       }
-     }
-    });
-    MenuItem closeItem = new MenuItem("Quit"); //File > Quit
-    closeItem.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent e) {
-        if (saveState)
-          primaryStage.close();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+            new ExtensionFilter("PNG", "*.png"));
+        File save = fileChooser.showSaveDialog(drawStage);
+        if (save != null) {
+          try {
+            ImageIO.write(SwingFXUtils.fromFXImage(drawScene.snapshot(null),
+                  null), "png", save);
+          } catch (Exception ex) {} //TODO: impelement catch
+        }
       }
     });
-    /* add items to file menu */
-    fileMenu.getItems().addAll(newItem, openItem, exportItem, closeItem);
 
-    /* 'edit' menu */
-/*    final Menu editMenu = new Menu("Edit");
-    MenuItem cpItem = new MenuItem("Copy");
-    MenuItem cutItem = new MenuItem("Cut");
-    MenuItem pItem = new MenuItem("Paste");
-    editMenu.getItems().addAll(cpItem, cutItem, pItem); */
+    /* add items to file menu */
+    fileMenu.getItems().addAll(openItem, exportItem);
 
     menuBar = new MenuBar(fileMenu); // compile menubar
   }
 
-  /* Main: not used. ensures compatibilty with swing apps */
   public static void main(String [] args) {
     launch(args);
   }
